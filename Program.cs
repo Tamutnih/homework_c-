6bc4fa01b97db4e0aabb865bcd7c8ffd600d2d4e
tatyana mutnykh <renu07@list.ru>
2022-08-06 22:18:36 +0500

    решение

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
+
+Console.Write("Число в квадрате " + sqrt);
+123
